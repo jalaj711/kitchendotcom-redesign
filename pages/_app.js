1b516923439fccf037abc4345701c29314e49e7e
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-09-09 21:40:01 +0530

    add sass
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.scss";
+import Head from "next/head";
+//import Footer from "../components/Footer/Footer";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Kitchendotcom</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      {/* <NavBar /> */}
+      <Component {...pageProps} />
+      {/* <Footer /> */}
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
