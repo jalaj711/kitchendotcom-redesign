ebf6acfcb42062a03cfecff1200ee2e45b664cc5
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-09-09 23:19:56 +0530

    create card component
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ components/Card/Card.js
@@ -0,0 +1,9 @@
+import styles from "./Card.module.scss";
+
+export default function Card(props) {
+  return (
+    <div className="styles.card" {...props}>
+      {props.children}
+    </div>
+  );
+}
