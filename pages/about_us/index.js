d01967825aa4dd6d76b35fceace644da49b9fd9c
Ayush Verma <itsmedoraemon19@gmail.com>
2022-09-11 01:15:27 +0530

    about us update

--- pages/about_us/index.js
+++ pages/about_us/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import Image from '../../assets/Rectangle213.jpg';
-import styles from './styles.module.scss'
+import React from "react";
+import Image from "../../assets/Rectangle213.jpg";
+import Image1 from "../../assets/Rectangle252.jpg";
+import styles from "./styles.module.scss";
 
 const About = () => {
   return (
@@ -20,14 +21,30 @@ const About = () => {
             modular kitchen with all the updated features.
           </p>
         </div>
-        <div className={styles.img1}>
-            <img src={Image.src}>
+        <div>
+          <img className={styles.img1} src={Image.src}></img>
+        </div>
+      </div>
+      <div className={styles.ourvision}>
+        <div>
+          <img className={styles.img1} src={Image1.src}></img>
+        </div>
+        <div className={styles.paracontainer}>
+          <h2 className={styles.vsn}>OUR VISION</h2>
 
-            </img>
+          <p>
+            <b>Our</b> vision is to be a reputable, sustainable, and desirable
+            premier modular kitchen service provider of the global marketplace
+            providing state-of-the-art innovative designs and superior quality
+            finishing. We aim to be able to give our best services in each city.
+            We seek to provide such services which make kitchen chores a
+            hassle-free task. We are a one-stop solution to all your needs for a
+            modular kitchen with all the updated features.
+          </p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default About;
