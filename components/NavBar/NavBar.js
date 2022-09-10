c8297396a28b846bb7e8b736de7e59801a63a97c
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-09-10 23:05:41 +0530

    add lamp to topleft navbar
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,15 +1,20 @@
 import styles from "./NavBar.module.scss";
+import LeftImage from "../../assets/NavBarLeft.png";
+import Image from "next/image";
 
 export default function NavBar() {
   return (
     <nav className={styles.nav}>
-      <span>Kitchendotcom</span>
-      <span style={{ flexGrow: 1 }} />
-      <span>Home</span>
-      <span>About Us</span>
-      <span>Blog</span>
-      <span>Cities</span>
-      <span>Contact us</span>
+      <Image src={LeftImage} alt="lamp" />
+      <div>
+        <span>Kitchendotcom</span>
+        <span style={{ flexGrow: 1 }} />
+        <span>Home</span>
+        <span>About Us</span>
+        <span>Blog</span>
+        <span>Cities</span>
+        <span>Contact us</span>
+      </div>
     </nav>
   );
 }
