85f9f7909a9af1f7d95d1226c02563f9ed65d459
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-09-20 10:32:37 +0530

    add secondary and large option to buttons
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -1,5 +1,5 @@
 import styles from "./Button.module.scss";
 
 export default function Button(props) {
-  return <button className={styles.btn} {...props}>{props.children}</button>;
+  return <button className={`${styles.btn} ${props.large && styles.large} ${props.secondary && styles.secondary}`} {...props}>{props.children}</button>;
 }
