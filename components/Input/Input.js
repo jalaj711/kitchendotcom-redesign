b4b90cdaae9720c8eaad1ed413c32c3c96f8e757
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-09-29 12:36:06 +0530

    add `small` and `border` props to Input
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,5 +1,12 @@
 import styles from "./Input.module.scss";
 
 export default function Input(props) {
-  return <input className={styles.inp} {...props} />;
+  return (
+    <input
+      className={`${styles.inp} ${props.small && styles.small} ${
+        props.border && styles.border
+      }`}
+      {...props}
+    />
+  );
 }
