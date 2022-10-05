a9c2b9638019a11e530a750780fc7d9ad51c55ed
Ayush Verma <itsmedoraemon19@gmail.com>
2022-10-05 22:41:04 +0530

    responsive 13,14,15,16

--- pages/estimator-15/index.js
+++ pages/estimator-15/index.js
@@ -3,80 +3,67 @@ import styles from "./styles.module.scss";
 import DoodleDots from "../../components/DoodleDotsEllipse";
 import Image1 from "../../assets/Rectangle15.png";
 import ThreeTriangles from "../../components/ThreeTriangles";
+import Button from "../../components/Button/Button";
 
-const Estimator14 = () => {
+const Estimator13 = () => {
   return (
     <div className={styles.container}>
       <DoodleDots className={styles.dots} />
       <div className={styles.head}>
         <h1 className={styles.est}> Kitchen Price Estimate Steps</h1>
       </div>
+
       <div className={styles.card}>
-        <ThreeTriangles className={styles.tri} />
-        <div className={styles.div1}>
-          <div className={styles.v1}>
-            <ThreeTriangles className={styles.tri1} />
-            <h2 className={styles.ess}>Luxe</h2>
+        <div className={styles.section}>
+          <div className={styles.b0}>
+            <h2>Luxe</h2>
             <p className={styles.ran}>
               Lixurious dream kitchen that’s a fine blend of aesthetics and high
               functionality complete with swanky units and accessories.
             </p>
-          </div>
-          <div className={styles.img}>
+
             <img className={styles.img1} src={Image1.src} />
-          </div>
-          <div className={styles.para}>
-            <h4>PRO TIP:</h4>
+
             <p className={styles.p1}>
-              Choose this if you want to go all out to ensure your dream luxury
-              kitchen gives you an exquisite culinary expierience that fits your
-              lifestyle.
+              <b>PRO TIP: </b>Choose this if you want to go all out to ensure
+              your dream luxury kitchen gives you an exquisite culinary
+              expierience that fits your lifestyle.
             </p>
           </div>
-        </div>
-        <div className={styles.div2}>
-          <div className={styles.v2}>
-            <div className={styles.pre}>
+
+          <div className={styles.sec2}>
+            <div className={styles.b1}>
               <h3>Essentials</h3>
               <p className={styles.p2}>
                 A range that has the basic unit and accessories necessary for a
                 comfortable modular kitchen.
               </p>
             </div>
-          </div>
-        </div>
-        <div className={styles.div3}>
-          <div className={styles.v3}>
-            <div className={styles.lux}>
+            <div className={styles.b1}>
               <h3>Premium</h3>
-              <p className={styles.p3}>
+              <p className={styles.p2}>
                 An exquisite kitchen with sleek fixtures, hardware, cabinets &
                 fittings for an elegant and smooth cooking experiences.
               </p>
             </div>
-          </div>
-        </div>
-        <div className={styles.div4}>
-          <div className={styles.v4}>
-            <div className={styles.lux}>
+            <div className={styles.b1}>
               <h3>Build your own package</h3>
-              <p className={styles.p4}>
+              <p className={styles.p2}>
                 Select materials, finishes, accessories and more of your choices
                 from a range of collections for your kitchen.
               </p>
             </div>
           </div>
         </div>
+        <div className={styles.buttons1}>
+          <Button secondary>Back</Button>
+          <Button>Next</Button>
+        </div>
       </div>
 
-      <div className={styles.buttons1}>
-        <button className={styles.back}>Back</button>
-
-        <button className={styles.next}>Next</button>
-      </div>
       <DoodleDots className={styles.dots1} />
     </div>
   );
 };
 
-export default Estimator14;
+export default Estimator13;
