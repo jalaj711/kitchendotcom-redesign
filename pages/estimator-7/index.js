d9f7f02d5040d5415aec230c4eba5868ba196440
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-10-08 17:04:01 +0530

    implement new designs for estimator 7
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- pages/estimator-7/index.js
+++ pages/estimator-7/index.js
@@ -4,38 +4,45 @@ import ThreeTriangles from "../../components/ThreeTriangles";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import Image from "../../assets/estimator/estimator-7-main.png";
+import LoginImage from "../../assets/estimator/7.png";
+
 const Estimater2 = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.head}>
-        <h1 className={styles.est}>Kitchen Price Estimator Step 3</h1>
-      </div>
-      <div className={styles.card}>
-        <div className={styles.kitchenTypeHolder}>
-          <div className={styles.imageWithSquare}>
-            <img src={Image.src} />
-          </div>
-          <div className={styles.rightBar}>
-            <h2>Please enter your details to get the details</h2>
-            <div className={styles.dataForm}>
-              <div>
-                <div>Your full name:</div>
-                <Input type="text" />
+      <div className={styles.backgroundCard}>
+        <div className={styles.head}>
+          <h1 className={styles.est}>Kitchen Price Estimator Step 3</h1>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.kitchenTypeHolder}>
+            <div className={styles.imageWithSquare}>
+              <img src={Image.src} />
+            </div>
+            <div className={styles.rightBar}>
+              <div className={styles.rightBarTitle}>
+                <h2>Please enter your details to get the details</h2>
+                <img src={LoginImage.src} />
               </div>
-              <div>
-                <div>Your phone number:</div>
-                <Input type="text" />
-              </div>
-              <div>
-                <div>Your email id:</div>
-                <Input type="text" />
+              <div className={styles.dataForm}>
+                <div>
+                  <div>Your full name:</div>
+                  <Input type="text" />
+                </div>
+                <div>
+                  <div>Your phone number:</div>
+                  <Input type="text" />
+                </div>
+                <div>
+                  <div>Your email id:</div>
+                  <Input type="text" />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className={styles.buttons}>
-          <Button secondary>Back</Button>
-          <Button>Next</Button>
+          <div className={styles.buttons}>
+            <Button secondary>Back</Button>
+            <Button>Next</Button>
+          </div>
         </div>
       </div>
 
