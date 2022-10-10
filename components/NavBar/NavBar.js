f819c16ff8564ad4409951e13b0fd241e640bbb9
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-10-10 18:37:09 +0530

    implement new designs for navbar
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,12 +1,8 @@
 import styles from "./NavBar.module.scss";
-import LeftImage from "../../assets/NavBarLeft.png";
-import Image from "next/image";
 
 export default function NavBar() {
   return (
     <nav className={styles.nav}>
-      <Image src={LeftImage} alt="lamp" />
-      <div>
         <span>Kitchendotcom</span>
         <span style={{ flexGrow: 1 }} />
         <span>Home</span>
@@ -14,7 +10,6 @@ export default function NavBar() {
         <span>Blog</span>
         <span>Cities</span>
         <span>Contact us</span>
-      </div>
     </nav>
   );
 }
