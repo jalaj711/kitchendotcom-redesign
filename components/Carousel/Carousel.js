41541e20a21ccfc9b2d9d18696b6c143c2b196aa
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-10-12 17:42:11 +0530

    create carousel component
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ components/Carousel/Carousel.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import styles from "./Carousel.module.scss";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const formatNumber = n =>  n > 9 ? n : '0' + n
+
+export default function CustomCarousel({ images }) {
+  const [current, setCurrent] = useState(0);
+  console.log(styles);
+  return (
+    <div className={styles.carousel}>
+      <div className={styles.images}>
+        <Carousel
+          showThumbs={false}
+          showIndicators={false}
+          autoPlay={true}
+          interval={3000}
+          infiniteLoop={true}
+          showStatus={false}
+          selectedItem={current}
+          onChange={(index) => setCurrent(index)}
+        >
+          {images.map((elem, index) => (
+            <div key={index} className={styles.leftImageContainer}>
+              <img src={elem} className={styles.leftImage} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      <div className={styles.controls}>
+        <button onClick={() => setCurrent(current - 1)} className={styles.prevButton}>&lt;</button>
+        <span className={styles.currentStatus}>
+          <b>{formatNumber(current)}</b>/{formatNumber(images.length)}
+        </span>
+        <button onClick={() => setCurrent(current + 1)} className={styles.nextButton}>&gt;</button>
+      </div>
+    </div>
+  );
+}
