debeaa2f8225c4e8515d787689298c0dbc4cfd49
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-10-25 14:12:06 +0530

    create seperate carousels for project gallery
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ pages/project-gallery/Carousel1.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import styles from "./Carousel.module.scss";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const formatNumber = (n) => (n > 9 ? n : "0" + n);
+
+export default function CustomCarousel({ images }) {
+  const [current, setCurrent] = useState(0);
+  return (
+    <div className={styles.carousel}>
+      <div className={styles.images}>
+        <Carousel
+          showThumbs={false}
+          showIndicators={false}
+          autoPlay={true}
+          interval={3000}
+          infiniteLoop={true}
+          showStatus={false}
+          selectedItem={current}
+          onChange={(index) => setCurrent(index)}
+        >
+          {images.map((elem, index) => (
+            <div key={index} className={styles.imageContainer}>
+              <img src={elem} className={styles.img} />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      <div className={styles.controls}>
+        <button
+          onClick={() => setCurrent(current - 1)}
+          className={styles.prevButton}
+        >
+          &lt;
+        </button>
+        <span className={styles.currentStatus}>
+          <b>{formatNumber(current + 1)}</b>/{formatNumber(images.length)}
+        </span>
+        <button
+          onClick={() => setCurrent(current + 1)}
+          className={styles.nextButton}
+        >
+          &gt;
+        </button>
+      </div>
+    </div>
+  );
+}
