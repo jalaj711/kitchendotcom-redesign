df5aa7a42d4fa58207546393ec2f5e4d22b15717
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-10-26 23:55:56 +0530

    add location option in customer details page
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- pages/estimator/customer-details/index.js
+++ pages/estimator/customer-details/index.js
@@ -10,23 +10,25 @@ import FetchFromApi from "../../../utils/fetchFromApi";
 import URLs from "../../../utils/urls";
 import { useRouter } from "next/router";
 
-
-
 const Estimater2 = () => {
-  const router=useRouter()
+  const router = useRouter();
   const handleClick = () => {
     var name = document.getElementById("name").value;
     var number = document.getElementById("number").value;
     var email = document.getElementById("email").value;
+    var location = document.getElementById("location").value;
 
     if (name && number && email) {
       FetchFromApi.post(URLs.ESTIMATOR_7, {
-        name,number,email,
+        name,
+        number,
+        email,
+        location
       }).then((res) => {
         if (res.status == 200) {
           return res.json().then((res) => {
             if (res.success) {
-              // router.push(res.next);
+              router.push(res.next);
             }
           });
         }
@@ -64,17 +66,43 @@ const Estimater2 = () => {
                   <div>Your email id:</div>
                   <Input type="text" id="email" />
                 </div>
+                <div>
+                  <div>Your location:</div>
+
+                  <select name="locations" id="location">
+                    {[
+                      "Varanasi",
+                      "Chandauli",
+                      "Mirzapur",
+                      "Sonbhadra",
+                      "Ayodhya",
+                      "Prayagraj",
+                      "Lucknow",
+                      "Bhadohi",
+                      "Gorakhpur",
+                      "Ghazipur",
+                      "Azamgarh",
+                      "Kanpur",
+                      "Jaunpur",
+                      "Other",
+                    ].map((elem, index) => (
+                      <option key={index} value={elem}>
+                        {elem}
+                      </option>
+                    ))}
+                  </select>
+                </div>
               </div>
             </div>
           </div>
           <div style={{ width: "100%" }} className={styles.buttons}>
-          <Button style={{ float: "left" }} secondary onClick={router.back}>
-            Previous
-          </Button>
-          <Button style={{ float: "right" }} onClick={handleClick}>
-            Next
-          </Button>
-        </div>
+            <Button style={{ float: "left" }} secondary onClick={router.back}>
+              Previous
+            </Button>
+            <Button style={{ float: "right" }} onClick={handleClick}>
+              Next
+            </Button>
+          </div>
         </div>
       </div>
       <TwoSquaresTopRight className={styles.square1} />
