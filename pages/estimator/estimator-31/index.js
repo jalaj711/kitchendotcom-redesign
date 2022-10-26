daaf70dcd0da8b4da8ff2da867e1b3274b9e9dd3
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-10-26 15:21:48 +0530

    finalize designs for page31
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- pages/estimator/estimator-31/index.js
+++ pages/estimator/estimator-31/index.js
@@ -1,16 +1,53 @@
 import React, { useState } from "react";
 import styles from "./styles.module.scss";
-import ThreeTriangles from "../../components/ThreeTriangles";
-import Button from "../../components/Button/Button";
-import Input from "../../components/Input/Input";
-import TwoSquares from "../../components/TwoSquares";
+import Button from "../../../components/Button/Button";
+import TwoSquaresTopRight from "../../../components/TwoSquaresTopRight";
+import TwoSquaresBottomLeft from "../../../components/TwoSquaresBottomLeft";
+import FetchFromApi from "../../../utils/fetchFromApi";
+import URLs from "../../../utils/urls";
+import { useRouter } from "next/router";
 
 const Estimater2 = () => {
+  const [active, setActive] = useState(-1);
+  const router = useRouter();
+
+  const handleClick = () => {
+    var kitchenLayout;
+    switch (active) {
+      case 0:
+        kitchenLayout = "L-Shaped";
+        break;
+      case 1:
+        kitchenLayout = "Straight";
+        break;
+      case 2:
+        kitchenLayout = "U-Shaped";
+        break;
+      case 3:
+        kitchenLayout = "Parallel";
+        break;
+    }
+    if (kitchenLayout) {
+      FetchFromApi.post(URLs.ESTIMATOR_2_6_SELECT_LAYOUT, {
+        kitchenLayout,
+      }).then((res) => {
+        if (res.status == 200) {
+          return res.json().then((res) => {
+            if (res.success) {
+              router.push(res.next);
+            }
+          });
+        }
+      });
+    } else {
+      alert("Please select one option");
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.backgroundCard}>
         <div className={styles.head}>
-          <h1 className={styles.est}>Kitchen Price Estimator Step 2</h1>
+          <h1 className={styles.est}>Kitchen Price Estimator Steps</h1>
         </div>
         <div className={styles.card}>
           <h2>Now choose the accessories for your kitchen.</h2>
@@ -58,29 +95,17 @@ const Estimater2 = () => {
             </div>
           </div>
         </div>
-        <div className={styles.buttons}>
-          <Button secondary>Back</Button>
-          <Button>Next</Button>
+        <div style={{ width: "100%" }} className={styles.buttons}>
+          <Button style={{ float: "left" }} secondary onClick={router.back}>
+            Previous
+          </Button>
+          <Button style={{ float: "right" }} onClick={handleClick}>
+            Next
+          </Button>
         </div>
       </div>
-
-      <div className={styles.decoratives}>
-        <TwoSquares
-          style={{ top: "90%", left: "-22vw", width: "max(150px, 50vw)" }}
-        />
-        <TwoSquares
-          style={{
-            top: "-4%",
-            right: "min(-50px, -20vw)",
-            width: "max(150px, 50vw)",
-          }}
-        />
-        <span style={{ top: "0%", left: "0%" }} className={styles.circle} />
-        <span style={{ top: "40%", left: "15%" }} className={styles.circle} />
-        <span style={{ top: "0%", left: "65%" }} className={styles.circle} />
-        <span style={{ top: "20%", left: "100%" }} className={styles.circle} />
-        <span style={{ top: "40%", left: "100%" }} className={styles.circle} />
-      </div>
+      <TwoSquaresTopRight className={styles.square1} />
+      <TwoSquaresBottomLeft className={styles.square2} />
     </div>
   );
 };
