fb6697a2ee38740645dd20ce9968afdf185b342e
Ayush Verma <itsmedoraemon19@gmail.com>
2022-10-26 13:15:01 +0530

    estimator-13 backend

--- pages/estimator-13/index.js
+++ pages/estimator-13/index.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import DoodleDots from "../../components/DoodleDotsEllipse";
 import Image1 from "../../assets/Rectangle91.png";
 import ThreeTriangles from "../../components/ThreeTriangles";
 import Button from "../../components/Button/Button";
 import TwoSquares from "../../components/TwoSquares";
+import FetchFromApi from "../../utils/fetchFromApi";
+import URLs from "../../utils/urls";
+import { useRouter } from "next/router";
 
 const Estimator13 = () => {
+  const [active, setActive] = useState(-1);
+  const router = useRouter();
+
+  const handleClick = () => {
+    var kitchenLayout;
+    switch (active) {
+      case 0:
+        kitchenLayout = "Essentials";
+        break;
+      case 1:
+        kitchenLayout = "Premium";
+        break;
+      case 2:
+        kitchenLayout = "Luxe";
+        break;
+      case 3:
+        kitchenLayout = "Build your own";
+        break;
+    }
+    if (kitchenLayout) {
+      FetchFromApi.post(URLs.ESTIMATOR_12, {
+        kitchenLayout,
+      }).then((res) => {
+        if (res.status == 200) {
+          return res.json().then((res) => {
+            if (res.success) {
+              router.push(res.next);
+            }
+          });
+        }
+      });
+    } else {
+      alert("Please select one option");
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.head}>
@@ -15,25 +53,38 @@ const Estimator13 = () => {
 
       <div className={styles.card}>
         <div className={styles.section}>
-          <div className={styles.b0}>
-            <h2>Essentials</h2>
-            <p className={styles.ran}>
-              A range that has the basic unit and accessories necessary for a
-              comfortable modular kitchen.
-            </p>
+          <div className={styles.b0} onClick={()=> setActive(0)}>
+            <div >
+              <span
+                className={`${styles.radio} ${
+                  active === 0 ? styles.radioActive : ""
+                }`}
+              ></span>
+            </div>
+            <div>
+              <h2>Essentials</h2>
+              <p className={styles.ran}>
+                A range that has the basic unit and accessories necessary for a
+                comfortable modular kitchen.
+              </p>
 
-            <img className={styles.img1} src={Image1.src} />
+              <img className={styles.img1} src={Image1.src} />
 
-            <p className={styles.p1}>
-              <b>PRO TIP: </b>If you’re lookingfor a kitchen with the necessary
-              units for an easycooking time this is yours pick!{" "}
-            </p>
+              <p className={styles.p1}>
+                <b>PRO TIP: </b>If you’re lookingfor a kitchen with the
+                necessary units for an easycooking time this is yours pick!{" "}
+              </p>
+            </div>
           </div>
 
           <div className={styles.sec2}>
-            <div className={styles.b1}>
+            <div className={styles.b1} onClick={() => setActive(1)}>
               <div>
-                <span className={styles.radio}></span>
+                <span
+                  className={`${styles.radio} ${
+                    active === 1 ? styles.radioActive : ""
+                  }`}
+                ></span>
               </div>
               <div>
                 <h3>Premium</h3>
@@ -44,8 +95,12 @@ const Estimator13 = () => {
               </div>
             </div>
             <div className={styles.b1}>
-              <div>
-                <span className={styles.radio}></span>
+              <div className={styles.choice} onClick={()=>setActive(2)}>
+                <span
+                  className={`${styles.radio} ${
+                    active === 2 ? styles.radioActive : ""
+                  }`}
+                ></span>
               </div>
               <div>
                 <h3>Luxe</h3>
@@ -56,8 +111,12 @@ const Estimator13 = () => {
               </div>
             </div>
             <div className={styles.b1}>
-              <div>
-                <span className={styles.radio}></span>
+              <div className={styles.choice} onClick={()=>{setActive(3);console.log('select')}}>
+                <span
+                  className={`${styles.radio} ${
+                    active === 3 ? styles.radioActive : ""
+                  }`}
+                ></span>
               </div>
               <div>
                 <h3>Build your own package</h3>
@@ -70,8 +129,10 @@ const Estimator13 = () => {
           </div>
         </div>
         <div className={styles.buttons1}>
-          <Button secondary>Back</Button>
-          <Button>Next</Button>
+          <Button secondary onClick={router.back}>
+            Back
+          </Button>
+          <Button onClick={handleClick}>Next</Button>
         </div>
       </div>
       <div className={styles.decoratives}>
--- utils/urls.js
+++ utils/urls.js
@@ -2,6 +2,7 @@ const URLs = {
     ESTIMATOR_2_6_SELECT_LAYOUT: '/api/select-layout',
     ESTIMATOR_7:'/api/estimator-7',
     ESTIMATOR_12:'/api/estimator-12',
+    ESTIMATOR_13:'/api/estimator-13',
     ESTIMATOR_8_11_SELECT_DIMENSIONS: '/api/select-dimensions'
 }
 
