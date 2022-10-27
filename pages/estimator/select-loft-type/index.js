25c2b33c716ee657092b2cb1165dbe41308c6f1d
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-10-27 12:03:31 +0530

    add functionality to select loft type page
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

---
+++ pages/estimator/select-loft-type/index.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+import Button from "../../../components/Button/Button";
+import Input from "../../../components/Input/Input";
+import TwoSquaresTopRight from "../../../components/TwoSquaresTopRight";
+import TwoSquaresBottomLeft from "../../../components/TwoSquaresBottomLeft";
+import FetchFromApi from "../../../utils/fetchFromApi";
+import URLs from "../../../utils/urls";
+import { useRouter } from "next/router";
+
+const Estimater2 = () => {
+  const [active, setActive] = React.useState("");
+  const router = useRouter();
+  const handleClick = () => {
+    var loft;
+    switch (active) {
+      case "custom":
+        loft = Number(document.getElementById("loft-length").value);
+        break;
+      case "2":
+        loft = 2;
+        break;
+      case "0":
+        loft = 0;
+        break;
+    }
+    if (active) {
+      FetchFromApi.post(URLs.ESTIMATOR_25_SELECT_LOFT, {
+        loft,
+      }).then((res) => {
+        if (res.status == 200) {
+          return res.json().then((res) => {
+            if (res.success) {
+              router.push(res.next);
+            }
+          });
+        }
+      });
+    } else {
+      alert("Please select one option");
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.backgroundCard}>
+        <div className={styles.head}>
+          <h1 className={styles.est}>Kitchen Price Estimator Steps</h1>
+        </div>
+        <div className={styles.card}>
+          <h2>Which type of loft are you looking for?</h2>
+          <div className={styles.kitchenTypeHolder}>
+            <div className={styles.row}>
+              <div
+                className={styles.choice}
+                onClick={() => setActive("custom")}
+              >
+                <div>
+                  <span
+                    className={`${styles.radio} ${
+                      active === "custom" ? styles.active : ""
+                    }`}
+                  />
+                </div>
+                <div className={styles.choiceContent}>
+                  <h4>Customized loft</h4>
+                  <div style={{ marginTop: 8 }}>
+                    <span>Feet: </span>
+                    <Input
+                      type="number"
+                      small
+                      border
+                      placeholder={0}
+                      id="loft-length"
+                      disabled={active !== "custom"}
+                      value={3}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.choice} onClick={() => setActive("2")}>
+                <div>
+                  <span
+                    className={`${styles.radio} ${
+                      active === "2" ? styles.active : ""
+                    }`}
+                  />
+                </div>
+                <div className={styles.choiceContent}>
+                  <h4>2 feet loft</h4>
+                  <div style={{ marginTop: 8 }}>(Recommended)</div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.row}>
+              <div className={styles.choice} onClick={() => setActive("0")}>
+                <div>
+                  <span
+                    className={`${styles.radio} ${
+                      active === "0" ? styles.active : ""
+                    }`}
+                  />
+                </div>
+                <div className={styles.choiceContent}>
+                  <h4>No loft</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style={{ width: "100%" }} className={styles.buttons}>
+          <Button style={{ float: "left" }} secondary onClick={router.back}>
+            Previous
+          </Button>
+          <Button style={{ float: "right" }} onClick={handleClick}>
+            Next
+          </Button>
+        </div>
+      </div>
+      <TwoSquaresTopRight className={styles.square1} />
+      <TwoSquaresBottomLeft className={styles.square2} />
+    </div>
+  );
+};
+
+export default Estimater2;
--- utils/urls.js
+++ utils/urls.js
@@ -1,10 +1,11 @@
 const URLs = {
   ESTIMATOR_2_6_SELECT_LAYOUT: "/backend/select-layout/",
-  ESTIMATOR_7: "/backend/estimator-7/",
+  ESTIMATOR_7: "/backend/customer-details/",
   ESTIMATOR_8_11_SELECT_DIMENSIONS: "/backend/select-dimensions/",
   ESTIMATOR_12: "/backend/estimator-12/",
   ESTIMATOR_13: "/backend/estimator-13",
   ESTIMATOR_14: "/backend/estimator-14",
+  ESTIMATOR_25_SELECT_LOFT: "/backend/select-loft/",
   ESTIMATOR_26_30_SELECT_FINISH: "/backend/select-finish/",
   ESTIMATOR_31_34_SELECT_ACCESSORIES: "/backend/select-accessories/",
   ESTIMATOR_35_SELECT_SERVICES: "/backend/select-services/",
