b618f4682f7b6e048881e223f2f400d9c97a5496
Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>
2022-12-22 13:08:12 +0530

    update mobile navbar styles
    Signed-off-by: Jalaj Kumar <96870071+jalaj711@users.noreply.github.com>

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import styles from "./NavBar.module.scss";
 import Hamburger from "../../assets/hamburger.svg";
 import Link from "next/link";
+import BG from "../../assets/design-gallery/top-bg.webp";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
   const [open, setOpen] = useState(false);
@@ -9,9 +11,11 @@ export default function NavBar() {
   const handleClose = (evt) => {
     const mobileNav = document.getElementById("mobile-overlay");
     if (evt.target === mobileNav) {
+      document.documentElement.style.overflowY = "auto";
       setOpen(false);
     }
   };
+  const router = useRouter();
   useEffect(() => {
     window.addEventListener("scroll", () => {
       const elem = document.getElementById("page-bg");
@@ -30,29 +34,22 @@ export default function NavBar() {
       <nav className={`${styles.nav} ${navbarColor && styles.blackNav}`}>
         <button
           className={styles.hamburger}
-          onClick={() => setOpen(true)}
+          onClick={() => {
+            document.documentElement.style.overflowY = "hidden";
+            setOpen(true);
+          }}
           title="show menu"
         >
           <img src={Hamburger.src} width="36" alt="" />
         </button>
-        <Link href="/">
-          Kitchendotcom
-        </Link>
+        <Link href="/">Kitchendotcom</Link>
         <span style={{ flexGrow: 1 }} />
         <div className={styles.hideOnSmall}>
-          <Link href="/">
-            Home
-          </Link>
-          <Link href="/about-us">
-            About Us
-          </Link>
-          <Link href="/blog">
-            Blog
-          </Link>
+          <Link href="/">Home</Link>
+          <Link href="/about-us">About Us</Link>
+          <Link href="/blog">Blog</Link>
           {/* <Link style={{color: navbarColor}} href="/cities">Cities</Link> */}
-          <Link href="/contact-us">
-            Contact us
-          </Link>
+          <Link href="/contact-us">Contact us</Link>
         </div>
       </nav>
       <div
@@ -62,21 +59,43 @@ export default function NavBar() {
         style={{ display: open ? "block" : "none" }}
       >
         <div className={styles.mobileNav}>
-          <Link href="/" onClick={() => setOpen(false)}>
-            Home
-          </Link>
-          <Link href="/about-us" onClick={() => setOpen(false)}>
-            About Us
-          </Link>
-          <Link href="/blog" onClick={() => setOpen(false)}>
-            Blog
-          </Link>
-          {/* <Link href="/cities" onClick={() => setOpen(false)}>
+          <div className={styles.mobileNavBg}>
+            <img src={BG.src} />
+            <div>
+              <h1>Kitchendotcom</h1>
+            </div>
+          </div>
+          <div className={styles.mobileNav}>
+            <li className={router.asPath == "/" ? styles.activeLink : ""}>
+              <Link href="/" onClick={() => setOpen(false)}>
+                Home
+              </Link>
+            </li>
+            <li
+              className={router.asPath == "/about-us" ? styles.activeLink : ""}
+            >
+              <Link href="/about-us" onClick={() => setOpen(false)}>
+                About Us
+              </Link>
+            </li>
+            <li className={router.asPath == "/blog" ? styles.activeLink : ""}>
+              <Link href="/blog" onClick={() => setOpen(false)}>
+                Blog
+              </Link>
+            </li>
+            {/* <Link href="/cities" className={router.asPath == "/" ? styles.activeLink : ""} onClick={() => setOpen(false)}>
             Cities
           </Link> */}
-          <Link href="/contact-us" onClick={() => setOpen(false)}>
-            Contact us
-          </Link>
+            <li
+              className={
+                router.asPath == "/contact-us" ? styles.activeLink : ""
+              }
+            >
+              <Link href="/contact-us" onClick={() => setOpen(false)}>
+                Contact us
+              </Link>
+            </li>
+          </div>
         </div>
       </div>
     </>
